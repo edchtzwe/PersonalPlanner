58f1f158db6f5f407292377ba2589315982267f0
Edmund Chong Tze Wei <7440820@gmail.com>
2021-03-07 00:25:55 +1100

    Dev_4
    - Changed the parent Controller class to AbstractController as the Controller class will be deprecated in 4.1 The latest Symfony version is 5.2, which has long since killed the Controller class
    
    - Added 2 placeholder twig templates for the detail view and the list view

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,19 +2,40 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// Controller is deprecated in 4.1, AbstractController is the way to go, so let's future proof ourselves
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class EventController extends Controller
+class EventController extends AbstractController
 {
-    /**
-     * @Route("/event", name="event")
-     */
+    // see app/config/routes.yaml
     public function index(): Response
     {
         return $this->render('event/index.html.twig', [
             'controller_name' => 'EventController',
+        ]);
+    }
+
+    /**
+     * @Route("/event/{id}", name="view_event", requirements={"id" = "\d+"})
+     */
+    public function viewEvent($id = 0): Response
+    {
+        // this method will do a find by $id, the pass in the appropriate params to the twig template
+        return $this->render('event/view_event.html.twig', [
+            'controller_name' => 'id',
+        ]);
+    }
+
+    /**
+     * @Route("/events/{page}", name="view_events", requirements={"page" = "\d+"})
+     */
+    public function viewEventList($page = 1): Response
+    {
+        // this method will do a find by $id, the pass in the appropriate params to the twig template
+        return $this->render('event/view_events.html.twig', [
+            'controller_name' => 'id',
         ]);
     }
 }
