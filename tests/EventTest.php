3ac2066c754ef4633029a58a24015c90975d1e17
Edmund Chong Tze Wei <7440820@gmail.com>
2021-03-12 10:38:32 +1100

    Dev_13
    - Test Event creation with the DOM Crawler WebTestCase object by submitting the Event creation form

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -5,6 +5,11 @@ namespace App\Tests;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+use App\Controller\EventController;
+use App\Entity\Event;
+
+use DateInterval;
+
 class EventTest extends WebTestCase
 {
     public function testHome()
@@ -34,20 +39,27 @@ class EventTest extends WebTestCase
         $this->assertTrue($client->getResponse()->isSuccessful());
     }
 
-    public function testEventCRUD()
-    {
-        $client = static::createClient();
-
-        // test create
-        $client->request("GET", "/event/create_event");
-        $this->assertTrue($client->getResponse()->isSuccessful());
-    }
-
     public function testAddNewEvent()
     {
         $client = static::createClient();
+        $client->followRedirects();
 
-        $client->request("GET", "/event/add_event");
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        $crawler = $client->request("GET", "/event/add_event");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $form = $crawler->selectButton("Submit")->form();
+
+        $form["event[title]"]->setValue("Unit Test title");
+        $form["event[description]"]->setValue("Unit Test description");
+        $form["event[location]"]->setValue("Unit Test location");
+        $form["event[priority]"]->setValue("*****");
+        $startDateTime = new \DateTime('@'.strtotime('now'));
+        $endDateTime   = new \DateTime('@'.strtotime('now'));
+        $endDateTime->add(new DateInterval('P10D'));
+        $form["event[start_time]"]->setValue($startDateTime->format('Y-m-d'));
+        $form["event[end_time]"]->setValue($endDateTime->format('Y-m-d'));
+
+        $crawler = $client->submit($form);
+        $this->assertContains("UNDER CONSTRUCTION", $client->getResponse()->getContent());
     }
 }
